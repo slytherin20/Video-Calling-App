30c832b4720e02b1c08dc79a4c0da3dbed8e28bc
Sonali Gupta <36319762+slytherin20@users.noreply.github.com>
2018-07-26 23:27:21 +0530

    updated app.js

--- public/app.js
+++ public/app.js
@@ -24,23 +24,7 @@ $(document).ready(function() {
     let peerConnection;
     callbutton.onclick = call;
     endbutton.onclick = endcall;
-    const server={
-        "iceServers":[
-          {
-                "urls": "stun:stun.l.google.com:19302"
-            },
-            {
-                "urls":"turn:192.168.1.156:3478",
-                "username":"sonali",
-                "credential":"@ttitude"
-            },
-            {
-                "urls":"turn:w3.xirsys.com:80?transport=udp",
-                "username": "65b1ebe2-8f32-11e8-9680-0dbe0cda4869",
-                "credential":"65b1ec5a-8f32-11e8-85ae-f83960d357c9"
-            }
-        ]
-    };
+   
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     let room=prompt("Enter room name:");
     console.log(room);
